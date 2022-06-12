73c6ea193ff5d7b31222a792e4c8763cdf977e49
macaos <51513686+macaos@users.noreply.github.com>
2022-06-13 00:02:00 +0900

    Make Prototype

--- web/src/view/Main.tsx
+++ web/src/view/Main.tsx
@@ -3,7 +3,19 @@ import React from 'react';
 const Main = () => {
     return (
         <div>
-            ss
+            <div className="box-insert">
+                Q : <input /><br></br>
+                A : <input />
+            </div>
+            <div className="box-current">
+                <div>Cards : 110</div>
+                <div>
+                    OK3 : 40 <br></br>
+                    OK2: 50<br></br>
+                    OK1: 10<br></br>
+                    OK0: 20<br></br>
+                </div>
+            </div>
         </div>
     );
 };
